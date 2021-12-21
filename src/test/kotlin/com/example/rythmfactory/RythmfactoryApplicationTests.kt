2f929e71dd5d2e1dac9bfe36b9d99d551d9cd23c
Luis Morán <luis.teiichi@gmail.com>
2021-12-21 17:32:57 +0100

    start projectç

---
+++ src/test/kotlin/com/example/rythmfactory/RythmfactoryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.rythmfactory
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RythmfactoryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
