ad5a6f0a143d96ddef5f7ee6e7d500e675cf93c0
Luis Morán <luis.teiichi@gmail.com>
2022-01-03 12:13:50 +0100

    New test

---
+++ src/test/kotlin/com/example/rythmfactory/controllers/HealthCheckControllerTest.kt
@@ -0,0 +1,12 @@
+package com.example.rythmfactory.controllers
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class HealthCheckControllerTest{
+    @Test
+    fun test1(){
+        assertThat(1==1);
+    }
+}
